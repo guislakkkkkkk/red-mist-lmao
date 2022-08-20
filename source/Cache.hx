4c554a92288e12ba02637d6b962bbbabce44aa51
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-08-20 15:01:24 -0300

    Update Cache.hx

--- source/Cache.hx
+++ source/Cache.hx
@@ -21,7 +21,7 @@ import haxe.Exception;
 import flixel.tweens.FlxEase;
 import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
-#if sys
+#if cpp
 import sys.FileSystem;
 import sys.io.File;
 #end
